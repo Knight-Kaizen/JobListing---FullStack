89c2a814eea3fbff84f52525686c2d2b36f8938f
Mohammed Nushoor Khan <auferstehung20@gmail.com>
2023-04-16 23:03:17 +0530

    modified boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+
 const app = express();
 
+
+
+
+//---------------MongoDB connection----------------
+const url = "phrh.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('mongo connected'))
+    .catch(err => console.log(err));
+//-------------------------------------------------
 app.get('/', (req,res)=>{
     res.send('Backend working');
 })
